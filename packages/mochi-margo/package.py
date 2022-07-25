7a914fdf7fa47b5c7336df2d0e0ca6f62bfa37e6
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2022-07-25 16:09:48 -0400

    Fix merge conflicts

--- packages/mochi-margo/package.py
+++ packages/mochi-margo/package.py
@@ -21,7 +21,7 @@ class MochiMargo(AutotoolsPackage):
     # change), you should also open a PR to propagate the change to the
     # upstream spack package as well:
     # https://github.com/spack/spack/tree/develop/var/spack/repos/builtin/packages/mochi-margo
-    version('develop', branch='main')
+    #version('develop', branch='main')
     version('0.9.5', sha256='b5c52477a82aa44a079f876cbb8166d0bce5a07a92bcf8a0c76670b245e728a3')
     version('0.9.4', sha256='4292e083c8375ab07bc6dd0b3b1ea2ce9c9dd864c27ac7f07c6913dcccecc746')
     version('0.9.3', sha256='1331423d4864349c3a9ec52b2114122659da310d5270fa1aea652e8ee48a0b3a')
@@ -45,7 +45,7 @@ class MochiMargo(AutotoolsPackage):
     version('0.4.4', sha256='2e2e6e2a8a7d7385e2fe204c113cb149f30847f0b1f48ec8dd708a74280bd89e')
     version('0.4.3', sha256='61a634d6983bee2ffa06e1e2da4c541cb8f56ddd9dd9f8e04e8044fb38657475')
     version('0.4.2', sha256='91085e28f50e373b9616e1ae5c3c8d40a19a7d3776259592d8f361766890bcaa')
-    version('develop-test', git='https://github.com/srini009/mochi-margo.git', branch='experimental')
+    version('develop', git='https://github.com/srini009/mochi-margo.git', branch='experimental')
 
     variant('pvar', default=False, description="extract performance data from Mercury")
 
@@ -58,8 +58,8 @@ class MochiMargo(AutotoolsPackage):
     depends_on('pkgconfig', type=("build"))
     depends_on('argobots@1.0:')
     # "breadcrumb" support not available in mercury-1.0
-    depends_on('mercury@2.1.0', when='@develop-test')
-    depends_on('mercury@develop', when='@develop-test +pvar')
+    depends_on('mercury@2.1.0', when='@develop')
+    depends_on('mercury@develop', when='@develop +pvar')
     depends_on('mercury@1.0.0:', type=("build", "link", "run"), when='@:0.5.1')
     depends_on('mercury@2.0.0:', type=("build", "link", "run"), when='@0.5.2:')
 
